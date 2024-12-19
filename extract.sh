33cf6d69ce8c0310836478231e5319bdf9619550
vanwinkleck <vanwinkleck@protonmail.com>
2024-12-19 11:30:35 -0600

    added audio extraction script and todo list

---
+++ extract.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+input=$1
+output=$2
+
+if [ ! -f "$1" ] || [ "$1" = '-h' ] || [ "$1" = '-help' ] || [ "$1" = '--help' ];then
+  echo '
+Extract the audio from a video file.
+
+Usage:
+
+<inputFileName.webm> <outputFileName.mp3>'
+  exit 1
+fi
+
+(ffmpeg -nostdin -i $input -q:a 0 -map a $output.mp3)
